96dfdec5b696030bf5037c406425fa08933cb0fb
ENZZO FERNANDES DAS CHAGAS <enzzof08@gmail.com>
2026-02-24 16:37:25 -0300

    Tudo feito

--- main.js
+++ main.js
@@ -1,11 +1,49 @@
 'use strict'
 function adicionarProduto(){
-    const produto = document.getElementById('produto')
+
+    if(produto.value === '' || codigo.value === ''|| quantidade.value === ''){
+        alert ('Elemento digitado é incorreto')
+    }else if(isNaN(quantidade.value) ){
+        alert ('Elemento digitado é incorreto')
+    }
+    else{
     const lista = document.getElementById('lista')
+    const produto = document.getElementById('produto')
+    const codigo = document.getElementById('codigo')
+    const quantidade = document.getElementById('quantidade')
+    
+    const spanCodigo = document.createElement('span')
+    const spanQtd = document.createElement('span')
+    const spanProduto = document.createElement('span')
 
-    const span = document.createElement('span')
-    span.textContent = produto.value 
-    span.className = 'bg-blue-200 px-8 py-2'
+    const div = document.createElement('div')
+    div.className = 'flex gap-2'
+    spanProduto.textContent = produto.value 
+    spanProduto.className = 'bg-yellow-200 px-8 py-2 rounded'
+    produto.value = ''
+    
+    div.appendChild(spanProduto)
+    
+    
 
-    lista.appendChild(span)
+    spanCodigo.textContent = codigo.value
+    spanCodigo.className = 'bg-blue-200 px-8 py-2 rounded'
+    codigo.value = ''
+
+    div.appendChild(spanCodigo)
+    
+
+    spanQtd.textContent = quantidade.value
+    spanQtd.className = 'bg-red-200 px-8 py-2 rounded'
+    quantidade.value = ''
+
+    div.appendChild(spanQtd)
+
+
+
+    lista.appendChild(div)
+    
+
+    }
+    
 }
